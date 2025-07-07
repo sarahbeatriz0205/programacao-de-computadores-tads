aa172fdc0cbe5e5b203ac1cc77e11293fa80d8e4
Sarah Beatriz <sarah.nascimento02052007@gmail.com>
2025-07-07 11:24:30 -0300

    mais questões

--- Lista8/meiavida.py
+++ Lista8/meiavida.py
@@ -1,18 +1,15 @@
 segundos, massa = map(int, input().split())
 contador_while = 0
 
-#enquanto massa for maior que 0.5, faça isso:
-while (massa > 0.5):
+#enquanto massa for maior ou igual que 0.5, faça isso:
+while (massa >= 0.5):
     massa = massa / 2
     contador_while += 1 #número de vezes que o while foi executado
 tempo_total = contador_while * segundos #tempo total
 
+
 dias = tempo_total // 86400
-horas = tempo_total // 3600
-if horas >= 24:
-    horas = horas % 24
-minutos = tempo_total // 60
-if minutos >= 60:
-    minutos = minutos % 60
-segundo = tempo_total % 60
+horas = (tempo_total % 86400) // 3600
+minutos = ((tempo_total % 86400) % 3600) // 60
+segundo = ((tempo_total % 86400) % 3600)  % 60
 print(dias, "dias","{:02d}:{:02d}:{:02d}".format(horas, minutos, segundo))
--- Lista8/pulasapo.py
+++ Lista8/pulasapo.py
@@ -1,5 +1,11 @@
 pulo_sapo, num_canos = map(int, input().split())
 altura_canos = list(map(int, input().split()))
-diferenca = altura_canos[1] - altura_canos[0]
+diferenca = 0
+mensagem = "YOU WIN"
 
-# while diferenca < pulo_sapo:
+for i in range(1, num_canos):
+    diferenca = abs(altura_canos[i] - altura_canos[i-1]) #não pode ter valor negativo
+    if diferenca > pulo_sapo:
+        mensagem = "GAME OVER"
+        break
+print(mensagem)
