9dd12a2d21231f24d0a402375abca88c702b4527
Sarah Beatriz <sarah.nascimento02052007@gmail.com>
2025-07-07 11:56:39 -0300

    menor primo

--- Lista8/menorprimo.py
+++ Lista8/menorprimo.py
@@ -1,2 +1,23 @@
 #menor número primo maior que o inserido
-n = int(input())
+n = int(input()) 
+proximo_n = n + 1 
+fim = False
+
+while fim == False:
+    continuar = False
+    if proximo_n <= 1:
+        continuar = True
+    elif proximo_n == 2:
+        continuar = False
+    else:    
+        for i in range(2, proximo_n):
+            if proximo_n % i == 0:
+                continuar = True
+                break
+        
+    if continuar == False:
+        fim = True
+    else:
+        proximo_n += 1 # 7
+
+print(proximo_n)
