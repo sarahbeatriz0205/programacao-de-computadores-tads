463d55ce4a3d4a1ce89435cf01dd925e3b83da63
Sarah Beatriz <sarah.nascimento02052007@gmail.com>
2025-07-09 10:18:06 -0300

    blobs

--- Lista8/blobs.py
+++ Lista8/blobs.py
@@ -1,15 +1,19 @@
 num_casos = int(input())
 comida_em_kg = []
 dias = []
-contador_dias = 0
+
+for i in range(0, num_casos): 
+    comida_em_kg.append(float(input()))  
 
 for i in range(0, num_casos):
-    comida_em_kg.append(float(input()))
-for i in range(0, num_casos):
-    while (comida_em_kg[i] >= 1):
-        comida_em_kg[i] = comida_em_kg[i] //2
-        contador_dias += 1
-    dias.append(contador_dias)
     contador_dias = 0
+    if comida_em_kg[i] <= 1.0: 
+        dias.append(0)
+    else: 
+        while comida_em_kg[i] > 1.0:
+            comida_em_kg[i] = comida_em_kg[i] / 2
+            contador_dias += 1
+        dias.append(contador_dias)
 
-[print(dia, "dias") for dia in dias]
+for dia in dias:
+    print(dia, "dias")
---
+++ Lista8/despertador.py
@@ -0,0 +1,1 @@
+h1, m1, h2, m2 = list(map(int, input().split())) #len(list) -> número de testes
