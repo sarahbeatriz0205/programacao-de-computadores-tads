3321ee7c6d0d3e70050d0352b5dca5acdca202ce
Sarah Beatriz <sarah.nascimento02052007@gmail.com>
2025-07-09 11:55:48 -0300

    tem erros

--- Lista8/despertador.py
+++ Lista8/despertador.py
@@ -1,1 +1,5 @@
 h1, m1, h2, m2 = list(map(int, input().split())) #len(list) -> número de testes
+
+for i in range(len(list)):
+    
+
---
+++ Lista8/senha2.py
@@ -0,0 +1,10 @@
+#ordem decrescente da qtd de óleo sobre cada tecla
+#a tecla com maior qtd de óleo será o 1º dígito
+
+qtd_oleo= []
+
+for i in range(10):
+    qtd_oleo.append(float(input()))
+
+for oleo in qtd_oleo:
+    if qtd_oleo[i] > 
